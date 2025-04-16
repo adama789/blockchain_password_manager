eda2ebd7c72eab3276738396c5fbd3b17708432b
adama789 <cedrq@tlen.pl>
2025-04-16 22:58:47 +0200

    Add workflow

--- tests/blockchain_password_manager.ts
+++ tests/blockchain_password_manager.ts
@@ -1,16 +1,29 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
-import { BlockchainPasswordManager } from "../target/types/blockchain_password_manager";
+import { Myproject } from "../target/types/blockchain_password_manager";
 
-describe("blockchain_password_manager", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+describe("myproject", () => {
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
-  const program = anchor.workspace.blockchainPasswordManager as Program<BlockchainPasswordManager>;
+  const program = anchor.workspace.myproject as Program<Myproject>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+  it("Initializes an account with a value", async () => {
+    const account = anchor.web3.Keypair.generate();
+
+    const tx = await program.methods
+      .initialize()
+      .accounts({
+        initialAccount: account.publicKey,
+        user: provider.wallet.publicKey,
+        // systemProgram: anchor.web3.SystemProgram.programId,
+      })
+      .signers([account])
+      .rpc();
+
     console.log("Your transaction signature", tx);
+
+    const fetchedValue = await program.account.init.fetch(account.publicKey);
+    console.log("Fetched value:", fetchedValue.value.toString());
   });
 });
