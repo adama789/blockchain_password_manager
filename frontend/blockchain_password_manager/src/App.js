0816275aef0e4a614d2fd06b28e10a57e0e9506e
adama789 <cedrq@tlen.pl>
2025-04-19 11:31:58 +0200

    Allow connecting the Phantom wallet

--- frontend/blockchain_password_manager/src/App.js
+++ frontend/blockchain_password_manager/src/App.js
@@ -1,22 +1,59 @@
+import React, { useState } from 'react'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [walletAddress, setWalletAddress] = useState(null)
+
+  window.onload = async function () {
+    try {
+      if (window.solana) {
+        if (window.solana.isPhantom) {
+          console.log('Phantom wallet found!')
+          const res = await window.solana.connect({ onlyIfTrusted: true })
+          console.log("Connected with Public Key:", res.publicKey.toString())
+          setWalletAddress(res.publicKey.toString())
+        }
+      } else {
+        alert("Wallet not found! Get a Phantom Wallet!")
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  const connectWallet = async () => {
+    try {
+      if (window.solana) {
+        const res = await window.solana.connect()
+        setWalletAddress(res.publicKey.toString())
+      } else {
+        alert("Wallet not found! Get a Phantom Wallet!")
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {!walletAddress && (
+          <div>
+            <button className='btn' onClick={connectWallet}>
+              Connect Wallet
+            </button>
+          </div>
+        )}
+        {walletAddress && (
+          <div>
+            <p>
+              Connected account : {' '}
+              <span className="address"> {walletAddress} </span>
+            </p>
+          </div>
+        )}
       </header>
     </div>
   );
