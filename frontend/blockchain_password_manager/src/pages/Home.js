871114e74d7014a652c05a7b6dbb455f7bfbe014
adama789 <cedrq@tlen.pl>
2025-09-01 22:25:30 +0200

    Init React Bits

--- frontend/blockchain_password_manager/src/pages/Home.js
+++ frontend/blockchain_password_manager/src/pages/Home.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import WalletConnect from "../components/WalletConnect";
 import { connectWallet } from "../blockchain/provider";
 import { Navigate } from "react-router-dom";
+import Particles from '../components/ReactBits/Particles/Particles';
 
 function Home() {
   const [walletAddress, setWalletAddress] = useState(null);
@@ -17,7 +18,18 @@ function Home() {
 
   return (
     <div>
-      <h1>Welcome to Password Manager</h1>
+      <div style={{ width: '100%', height: '700px', position: 'relative', backgroundColor: '#060010' }}>
+        <Particles
+          particleColors={['#ffffffff', '#ffffffff']}
+          particleCount={200}
+          particleSpread={10}
+          speed={0.1}
+          particleBaseSize={100}
+          moveParticlesOnHover={true}
+          alphaParticles={false}
+          disableRotation={false}
+        />
+      </div>
       <WalletConnect onConnect={handleConnect} walletAddress={walletAddress} />
     </div>
   );
