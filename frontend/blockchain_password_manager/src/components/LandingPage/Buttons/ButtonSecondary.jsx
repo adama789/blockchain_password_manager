a500ba01e7df7568f1d78742f147ab9c3a7ea67a
adama789 <cedrq@tlen.pl>
2025-09-02 23:38:36 +0200

    Small tweaks to SecondaryButton

--- frontend/blockchain_password_manager/src/components/LandingPage/Buttons/ButtonSecondary.jsx
+++ frontend/blockchain_password_manager/src/components/LandingPage/Buttons/ButtonSecondary.jsx
@@ -4,9 +4,10 @@ function ButtonSecondary({ children, onClick }) {
   return (
     <button
       onClick={onClick}
-      className="px-8 py-4 rounded-full border border-primary text-primary 
-                 bg-dark/50 hover:bg-primary/10 hover:text-white
-                 hover:scale-105 transition-transform duration-200 font-semibold"
+      className="px-8 py-4 text-sm rounded-full 
+                 bg-primary/10 border border-primary/30 text-primary 
+                 backdrop-blur-sm font-semibold
+                 hover:bg-primary/20 hover:scale-105 transition-transform duration-200"
     >
       {children}
     </button>
