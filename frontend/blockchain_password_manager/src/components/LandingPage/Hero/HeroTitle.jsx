f692d9c8aab3c0513a9b1fa0866974b1513cc39f
adama789 <cedrq@tlen.pl>
2025-09-02 23:28:01 +0200

    Fix glow effect

--- frontend/blockchain_password_manager/src/components/LandingPage/Hero/HeroTitle.jsx
+++ frontend/blockchain_password_manager/src/components/LandingPage/Hero/HeroTitle.jsx
@@ -4,11 +4,13 @@ function HeroTitle() {
   return (
     <h1
       style={{ lineHeight: "1.2" }}
-      className="drop-shadow-[0_0_20px_rgba(168,85,247,0.6)] text-white text-4xl sm:text-5xl lg:text-6xl font-extrabold mb-10 max-w-4xl"
+      className="text-white text-4xl sm:text-5xl lg:text-6xl font-extrabold mb-10 max-w-4xl"
     >
-      Decentralized Password Manager
+      <span className="drop-shadow-[0_0_20px_rgba(100,100,100,100.6)]">
+        Decentralized Password Manager
+      </span>
       <br />
-      <span className="bg-gradient-to-r from-primary to-accent 
+      <span className="drop-shadow-[0_0_20px_rgba(168,85,247,0.6)] bg-gradient-to-r from-primary to-accent 
                        text-transparent bg-clip-text">
         on Solana Blockchain
       </span>
