a72f1b670db0563b585889cdb1fb99dd29949259
adama789 <cedrq@tlen.pl>
2025-09-02 10:33:51 +0200

    Fix spacing

--- frontend/blockchain_password_manager/src/components/LandingPage/Hero/HeroTitle.jsx
+++ frontend/blockchain_password_manager/src/components/LandingPage/Hero/HeroTitle.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function HeroTitle() {
   return (
-    <h1 className="text-4xl sm:text-5xl lg:text-6xl font-extrabold leading-snug mb-10 max-w-4xl">
+    <h1 style={{ lineHeight: '1.2' }} className="text-4xl sm:text-5xl lg:text-6xl font-extrabold leading-snug mb-10 max-w-4xl">
       Decentralized Password Manager
       <br />
       <span className="bg-gradient-to-r from-primary via-secondary to-accent text-transparent bg-clip-text">
